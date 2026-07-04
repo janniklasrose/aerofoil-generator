c3f8adc8abafba199a5ffa689216af8cec35972c
Jan N Rose <janniklas.rose@gmail.com>
2026-07-04 16:54:39 +0200

    Align naca5series with naca4series formatting

--- bin/naca5series.py
+++ bin/naca5series.py
@@ -7,7 +7,7 @@ For usage, run naca5series with the -h/--help option.
 Definition: https://en.wikipedia.org/wiki/NACA_airfoil#Five-digit_series
 See also: NACA-TR-537 and NACA-TR-610.
 
-Copyright (c) 2019 Jan Niklas Rose
+Copyright (c) 2026 Jan Niklas Rose
 """
 
 ### LIBRARIES ###
@@ -17,36 +17,42 @@ import argparse  # https://docs.python.org/2/library/argparse.html
 import csv       # https://docs.python.org/2/library/csv.html
 import math      # https://docs.python.org/2/library/math.html
 
+### CONSTANTS ###
+
+# mean line tables for design lift coefficient cld = 0.3
+# table[(P, Q)] = (r, k1)
+STANDARD_MEANLINES = {
+    ('1', '0'): (0.0580, 361.4  ), #  5%
+    ('2', '0'): (0.1260,  51.64 ), # 10%
+    ('4', '0'): (0.2900,   6.643), # 20%
+    ('3', '0'): (0.2025,  15.957), # 15%
+    ('5', '0'): (0.3910,   3.230), # 25%
+}
+REFLECT_MEANLINES = {
+    # ('1', '1') = NACA 211 was not published in NACA-TR-537.
+    ('2', '1'): (0.1300, 51.990), # 10%
+    ('3', '1'): (0.2170, 15.793), # 15%
+    ('4', '1'): (0.3180,  6.520), # 20%
+    ('5', '1'): (0.4410,  3.191), # 25%
+}
+MEANLINE_TABLE = {**STANDARD_MEANLINES, **REFLECT_MEANLINES}
+
 
 ### PARSE ARGUMENTS ###
 
-# mean line tables for design lift coefficient cld = 0.3
-# table[(P, Q)] = (r, k1, description)
-# P is the second NACA digit; Q is the reflex flag (third digit).
-MEANLINE_TABLE = {
-    ('1', '0'): (0.0580, 361.4, '05% standard'),
-    ('2', '0'): (0.1260, 51.64, '10% standard'),
-    ('3', '0'): (0.2025, 15.957, '15% standard'),
-    ('4', '0'): (0.2900, 6.643, '20% standard'),
-    ('5', '0'): (0.3910, 3.230, '25% standard'),
-    # Reflex mean lines. NACA 211 was not published in NACA-TR-537.
-    ('2', '1'): (0.1300, 51.990, '10% reflex'),
-    ('3', '1'): (0.2170, 15.793, '15% reflex'),
-    ('4', '1'): (0.3180, 6.520, '20% reflex'),
-    ('5', '1'): (0.4410, 3.191, '25% reflex'),
-}
-
-
+# validator for NACA 5-series definition
 def valid_naca5(string):
     """Validate a NACA 5-series definition."""
     if (len(string) != 5) or (not string.isdigit()):
         msg = "%r is not a 5 digit NACA aerofoil definition" % string
         raise argparse.ArgumentTypeError(msg)
     if (string[1], string[2]) not in MEANLINE_TABLE:
-        msg = "%r is not a supported NACA 5-series mean line" % string[:3]
+        msg = "%r is not a supported NACA 5-series mean line (leading 3 digits)" % string
+        raise argparse.ArgumentTypeError(msg)
+    if (string[0] == '0') and (string[2] != '0'):
+        msg = "%r is symmetric (digit 1) and must not have camber (digit 3)" % string
         raise argparse.ArgumentTypeError(msg)
     return string
-
 
 # command line parser
 parser = argparse.ArgumentParser(
@@ -112,64 +118,18 @@ def linspace(a, b, N=100):
     if N == 1:
         return b
     h = (b - a) / (N - 1)
-    return [a + h*n for n in range(N)]
-
-
+    return [a + h*n  for n in range(N)]
 def cos(x):
     # using https://docs.python.org/2/library/functions.html#map
     return map(math.cos, x)
-
-
 def dealzeros(N, M):
     return [[0]*N for _ in range(M)]
 
-
-def standard_meanline(x_c, r, k1):
-    """Return y_c and dy_c/dx for a standard NACA 5-series mean line."""
-    if 0 <= x_c < r:
-        y_c = k1/6.0 * (x_c**3 - 3*r*x_c**2 + r**2*(3 - r)*x_c)
-        dycdx = k1/6.0 * (3*x_c**2 - 6*r*x_c + r**2*(3 - r))
-    else:
-        y_c = k1/6.0 * r**3 * (1 - x_c)
-        dycdx = -k1/6.0 * r**3
-    return y_c, dycdx
-
-
-def reflex_meanline(x_c, r, k1, p):
-    """Return y_c and dy_c/dx for a reflexed NACA 5-series mean line."""
-    k2_k1 = (3*(r - p)**2 - r**3) / ((1 - r)**3)
-    if 0 <= x_c < r:
-        y_c = k1/6.0 * (
-            (x_c - r)**3
-            - k2_k1*(1 - r)**3*x_c
-            - r**3*x_c
-            + r**3
-        )
-        dycdx = k1/6.0 * (
-            3*(x_c - r)**2
-            - k2_k1*(1 - r)**3
-            - r**3
-        )
-    else:
-        y_c = k1/6.0 * (
-            k2_k1*(x_c - r)**3
-            - k2_k1*(1 - r)**3*x_c
-            - r**3*x_c
-            + r**3
-        )
-        dycdx = k1/6.0 * (
-            3*k2_k1*(x_c - r)**2
-            - k2_k1*(1 - r)**3
-            - r**3
-        )
-    return y_c, dycdx
-
-
 # camberline x-coordinates
 if args.spacing == 'cos':
-    x = [(1.0-xi)/2.0 for xi in cos(linspace(0.0, math.pi, args.res))]
+    x = [(1.0-xi)/2.0  for xi in cos(linspace(0.0, math.pi, args.res))]
 elif args.spacing == '2cos':
-    x = [1.0-xi for xi in cos(linspace(0.0, math.pi/2.0, args.res))]
+    x = [1.0-xi  for xi in cos(linspace(0.0, math.pi/2.0, args.res))]
 elif args.spacing == 'lin':
     x = linspace(0.0, 1.0, args.res)
 
@@ -180,30 +140,62 @@ Q = args.LPQTT[2]
 T = float(args.LPQTT[3:5])/100  # maximum thickness
 cld = 0.15 * L                  # design lift coefficient
 p = 0.05 * P                    # position of maximum camber
-r, k1_base, _ = MEANLINE_TABLE[(args.LPQTT[1], args.LPQTT[2])]
+r, k1_base = MEANLINE_TABLE[(args.LPQTT[1], args.LPQTT[2])]
 k1 = k1_base * (cld / 0.3)      # tabulated k1 values are for cld = 0.3
 
-# calculate
+# calculate 
 Npts = len(x)
 x_C, y_C, x_U, y_U, x_L, y_L = dealzeros(Npts, 6)  # initialise all
 for i in range(Npts):
     x_c = x[i]
 
-    # thickness
-    y_t = T/0.20 * (0.2969 * math.sqrt(x_c)
-                    - 0.1260 * x_c
-                    - 0.3516 * x_c**2
-                    + 0.2843 * x_c**3
-                    - 0.1015 * x_c**4)
+    # thickness (same as naca4series)
+    y_t = T/0.20 * ( 0.2969 * math.sqrt(x_c)
+                   - 0.1260 * x_c
+                   - 0.3516 * x_c**2
+                   + 0.2843 * x_c**3
+                   - 0.1015 * x_c**4 )
 
     # mean camber line
     if cld == 0:
+        # no lift means symmetrical and thus no camber
         y_c = 0
         dycdx = 0
     elif Q == '0':
-        y_c, dycdx = standard_meanline(x_c, r, k1)
+        # standard mean line
+        if 0 <= x_c < r:
+            y_c = k1/6.0 * (x_c**3 - 3*r*x_c**2 + r**2*(3 - r)*x_c)
+            dycdx = k1/6.0 * (3*x_c**2 - 6*r*x_c + r**2*(3 - r))
+        else:
+            y_c = k1/6.0 * r**3 * (1 - x_c)
+            dycdx = -k1/6.0 * r**3
     else:
-        y_c, dycdx = reflex_meanline(x_c, r, k1, p)
+        # reflex mean line
+        k2_k1 = (3*(r - p)**2 - r**3) / ((1 - r)**3)  # from NACA-TR-537
+        if 0 <= x_c < r:
+            y_c = k1/6.0 * (
+                (x_c - r)**3
+                - k2_k1*(1 - r)**3*x_c
+                - r**3*x_c
+                + r**3
+            )
+            dycdx = k1/6.0 * (
+                3*(x_c - r)**2
+                - k2_k1*(1 - r)**3
+                - r**3
+            )
+        else:
+            y_c = k1/6.0 * (
+                k2_k1*(x_c - r)**3
+                - k2_k1*(1 - r)**3*x_c
+                - r**3*x_c
+                + r**3
+            )
+            dycdx = k1/6.0 * (
+                3*k2_k1*(x_c - r)**2
+                - k2_k1*(1 - r)**3
+                - r**3
+            )
 
     # coordinates
     theta = math.atan(dycdx)
@@ -244,7 +236,7 @@ for i in range(2*Npts):  # 2x for both lower and upper
             y_i = y_U[-1-i]
         elif i == Npts:
             continue  # skip this one (duplicate LE)
-            # NOTE: TE is not closed by definition
+            #NOTE: TE is not closed by definition
         else:
             # go forward
             x_i = x_L[i-Npts]
